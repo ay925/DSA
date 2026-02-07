2c8f4814c5679c5fc8572e5b9d86d094dae08aec
Anupam Yadav <anupamyadav4347@gmail.com>
2026-02-07 18:43:02 +0530

    .

---
+++ 18_quickSort.py
@@ -0,0 +1,24 @@
+def partition(nums,low,high):
+    pivot=nums[low]
+    i,j=low,high
+    while i<j:
+        while nums[i]<=pivot and i<=high-1:
+            i+=1
+        while nums[j]>=pivot and j>=low+1:
+            j-=1
+        if i<j:
+            nums[i],nums[j]=nums[j],nums[i]
+    nums[low],nums[j]=nums[j],nums[low]
+    return j
+
+def quick_sort(nums,low,high):
+    if low<high:
+        p_ind=partition(nums,low,high)
+        quick_sort(nums,low,p_ind-1)
+        quick_sort(nums,p_ind+1,high)
+    return nums
+nums=[3,1,2,4,7,8]
+n=len(nums)
+print(quick_sort(nums,0,n-1))
+
+    
