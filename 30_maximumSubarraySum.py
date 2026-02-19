3ef4991482286770f2625b715bf3a71579d89fde
Anupam Yadav <anupamyadav4347@gmail.com>
2026-02-19 18:43:42 +0530

    .

--- 30_maximumSubarraySum.py
+++ 30_maximumSubarraySum.py
@@ -1,13 +1,28 @@
 # Brute force solution
-nums=[-2,1,-3,4,-1,2,3,-5,4]
+nums=[-2,1,-3,4,-1,2,1,-5,4]
 n=len(nums)
-total=nums[0]
 maxi=float("-inf")
 for i in range(n):
-    for j in range(i+1,n):
+    total=0
+    for j in range(i,n):
         total+=nums[j]
-    if total>maxi:
-        maxi=total
-    total=nums[i]
+        if total>maxi:
+            maxi=total
 
 print(maxi)
+
+#Optimal Solution
+nums=[-2,1,-3,4,-1,2,1,-5,4]
+n=len(nums)
+total=0
+maxi=float("-inf")
+a=[]
+for i in range(n):
+    total+=nums[i]
+    if total>maxi:
+        maxi=total
+    if total<0:
+        total=0
+    a.append(maxi)
+print(maxi)
+print(a)
