f1db97c4b71b232c25fb49131fa558feb0782880
Anupam Yadav <anupamyadav4347@gmail.com>
2026-03-07 22:41:20 +0530

    .

---
+++ 47_Operation,Traversal,Insertion,Deletion,Append(Linked_list).py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+    
+node1=Node(4)
+node2=Node(10)
+node3=Node(50)
+node4=Node(20)
+
+node1.next=node2
+node2.next=node3
+node3.next=node4
+
+print(node1.next.next.next.val)
