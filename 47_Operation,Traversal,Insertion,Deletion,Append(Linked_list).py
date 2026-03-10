9385dd519caeaa6d8bcdc979232ba4035c712b6c
Anupam Yadav <anupamyadav4347@gmail.com>
2026-03-10 22:46:01 +0530

    .

--- 47_Operation,Traversal,Insertion,Deletion,Append(Linked_list).py
+++ 47_Operation,Traversal,Insertion,Deletion,Append(Linked_list).py
@@ -3,16 +3,15 @@ class Node:
         self.val=val
         self.next=None
     
-node1=Node(4)
-node2=Node(10)
-node3=Node(50)
-node4=Node(20)
+# node1=Node(4)
+# node2=Node(10)
+# node3=Node(50)
+# node4=Node(20)
 
-node1.next=node2
-node2.next=node3
-node3.next=node4
+# node1.next=node2
+# node2.next=node3
+# node3.next=node4
 
-print(node1.next.next.val)
 
 
 class SinglyLinkedList:
@@ -27,4 +26,24 @@ class SinglyLinkedList:
             while(curr.next is not None):
                 curr=curr.next
             curr.next=newNode
+
+    def traversal(self):
+        if self.head is None:
+            print("SLL is empty")
+        else:
+            curr= self.head
+            while curr is not None:
+                print(curr.val,end=" ")
+                curr=curr.next
+
+sll=SinglyLinkedList()
+sll.append(5)
+sll.append(10)
+sll.append(15)
+sll.append(30)
+sll.append(40)
+sll.append(50)
+sll.append(80)
+sll.append(1000)
+sll.traversal()
 
